7bf0113c4fede35ec93fbdc0c46b26ad5c8dd42d
Severak <strajt9@seznam.cz>
2017-10-20 16:51:17 +0200

    fix mapdef

--- lualibs/mapdef.lua
+++ lualibs/mapdef.lua
@@ -10,6 +10,18 @@ local mapdef = {
 
 _G.ANY = mapdef.ANY
 
+function _G.stylesheet(name)
+	mapdef.name = name
+end
+
+function _G.by(author)
+	mapdef.author = author
+end
+
+function _G.background(color)
+	mapdef.background = color
+end
+
 function _G.line(def)
 	def._type = 'line'
 	def.color = def.color or error 'Missing color!'
